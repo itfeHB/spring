95416183e10e28c686a7f69cc308ea3ad54faa59
mac <mac@mac-PC>
2019-02-07 00:23:26 +0900

    aop annotation aspectj around

--- sts-aop/src/main/java/com/bit/sts01/aop/MyAop02Around.java
+++ sts-aop/src/main/java/com/bit/sts01/aop/MyAop02Around.java
@@ -3,9 +3,13 @@ package com.bit.sts01.aop;
 import java.util.Arrays;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
 
+@Aspect
 public class MyAop02Around {
 
+	 @Around("execution(* com.bit.sts01.model.JavaDao.func03(..))")
 	public void logAround(ProceedingJoinPoint joinPoint) throws Throwable {
 
 		System.out.println("logAround() is running!");
